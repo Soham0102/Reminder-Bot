83a3ae3e7736cbc31d255e26a943075a9a9bfec3
Soham0102 <sohamtawari1@gmail.com>
2025-02-06 10:23:40 +0530

    Reminder.py

--- Reminder.py
+++ Reminder.py
@@ -99,6 +99,4 @@ def main():
 
 
 if __name__ == "__main__":
-   loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(main())
+   asyncio.get_event_loop().run_until_complete(main())
